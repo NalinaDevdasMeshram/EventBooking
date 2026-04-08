c6115d7147ae9576f1526798dbd6750ae7f1c2cf
nalina <naleenameshram85@gmail.com>
2026-04-08 17:45:43 +0530

    working

--- Booking/src/pages/Home.jsx
+++ Booking/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Herosection from "../components/Herosection";
+import CardSection from "../components/CardSection";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Herosection />
+      <CardSection />
     </div>
   );
 };
